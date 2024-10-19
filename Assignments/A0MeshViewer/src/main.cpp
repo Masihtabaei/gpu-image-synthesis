74b9e861354335ab3252627499192877af20fa31
Seyedmasih Tabaei <masihtabaei@gmail.com>
2024-10-19 17:34:09 +0200

    Did a contribution to #4 and #5 by adding some comments

--- Assignments/A0MeshViewer/src/main.cpp
+++ Assignments/A0MeshViewer/src/main.cpp
@@ -7,13 +7,19 @@ using namespace gims;
 
 int main(int /* argc*/, char /* **argv */)
 {
+  // Instantiating a configuration for our D3D12-app
   gims::DX12AppConfig config;
+  // Changing the title of the window
   config.title    = L"Coburg Mesh Viewer";
+  // Deactivating the vertical synchronization
   config.useVSync = false;
+  // Activating the creation of the debug context
   config.debug    = true;
   try
   {
+    // Instantiating the mesh viewer
     MeshViewer meshViewer(config);
+    // Running the mesh viewer
     meshViewer.run();
   }
   catch (const std::exception& exceptionThrown)
